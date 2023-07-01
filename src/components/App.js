fb60909106252835bac8028513971a143c87f9ba
Giselle-97 <giselle.cduran@hotmail.com>
2023-07-01 19:49:11 +0200

    added filter name

--- src/components/App.js
+++ src/components/App.js
@@ -7,12 +7,32 @@ import '../styles/App.scss';
 
 function App() {
   const [charactersList, setCharactersList] = useState([]);
+  const [searchName, setSearchName] = useState('');
+  const [searchSpecie, setSearchSpecie] = useState('');
   //1-Fetch
   useEffect(() => {
     getDataApi().then((cleanData) => {
       setCharactersList(cleanData);
     });
   }, []);
+
+  //function
+  const handleInputSearchName = (ev) => {
+    setSearchName(ev.target.value);
+  };
+
+  const handleInputSearchSpecie = (ev) => {
+    setSearchSpecie(ev.target.value);
+  };
+
+  const filterCharacters = charactersList.filter(
+    (eachCard) =>
+      eachCard.name.toLowerCase().includes(searchName.toLowerCase()).filter
+  );
+
+  /* const filterSpecies = charactersList.filter((eachCard) =>
+    eachCard.species.toLowerCase().includes(searchSpecie.toLowerCase())
+  );*/
 
   return (
     <div>
@@ -21,14 +41,37 @@ function App() {
         <p className='header__title'>Rick and Morty</p>
       </header>
       <main>
-        <form>
-          <label htmlFor=''></label>
-          <input type='text' placeholder='Buscar personaje' />
+        <form className='form'>
+          <div className='container'>
+            <label className='container__search' htmlFor=''>
+              Buscar personaje
+            </label>
+            <input
+              className='container__search--input'
+              type='text'
+              placeholder='Ej: Rick'
+              value={searchName}
+              onInput={handleInputSearchName}
+            />
+          </div>
+          <div className='container'>
+            <label className='container__search2' htmlFor=''>
+              Buscar especie
+            </label>
+            <input
+              className='container__search2--input'
+              type='text'
+              placeholder='Ej: Human'
+              value={searchSpecie}
+              onInput={handleInputSearchSpecie}
+            />
+          </div>
         </form>
-        <CharacterList charactersList={charactersList} />
+        <CharacterList charactersList={filterCharacters} />
       </main>
-      <footer>
-        <img src={imgLogo} alt='' />
+      <footer className='footer'>
+        <img className='footer__img' src={imgLogo} alt='' />
+        <p className='footer__text'>Adalab &copy;2023</p>
       </footer>
     </div>
   );
